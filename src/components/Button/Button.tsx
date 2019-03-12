c81a5238120eca6f837bd4918171f3dd377d5152
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-03-12 20:32:35 +0500

    fix(Button): Fixed Button component to recognise colors

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,18 +1,37 @@
+import { ThemeConsumer, ThemeContextData } from '@bluebase/core';
 import { Button as RNP_Button } from 'react-native-paper';
 import { ButtonProps } from '@bluebase/components';
-import { componentMapper } from '@bluebase/component-mapper';
+import React from 'react';
+// import { componentMapper } from '@bluebase/component-mapper';
 
-export const Button = componentMapper<ButtonProps>(RNP_Button, {
-	mode: 'variant',
+// export const Button = componentMapper<ButtonProps>(RNP_Button, {
+// 	mode: 'variant',
 
-	// TODO: fix color support
-	// color: 'color',
-	// disabled: 'disabled',
-	// fullWidth: 'fullWidth',
-	// size: 'size',
-	// variant: 'variant',
-}, {
-	ignore: ['color'],
-	rest: true,
-});
+// 	// TODO: fix color support
+// 	// color: 'color',
+// 	// disabled: 'disabled',
+// 	// fullWidth: 'fullWidth',
+// 	// size: 'size',
+// 	// variant: 'variant',
+// }, {
+// 	ignore: ['color'],
+// 	rest: true,
+// });
 
+
+const ButtonComponent = (props: ButtonProps) => (
+	<ThemeConsumer>
+		{({ theme }: ThemeContextData) => {
+
+			const { children, color, variant, ...rest } = props;
+			const rnpColor = (color === 'secondary') ? theme.palette.secondary.main : theme.palette.primary.main;
+
+			return (
+				<RNP_Button {...rest} mode={variant} color={rnpColor}>{children}</RNP_Button>
+			);
+		}}
+	</ThemeConsumer>
+);
+
+export const Button = ButtonComponent;
+// export const Button = React.memo(ButtonComponent);
