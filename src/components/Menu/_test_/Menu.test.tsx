223fd4a95a8567bddfe672da81cf16b73b93bd7e
adnan1naeem <adnan1naeem@gmail.com>
2019-04-16 17:09:47 +0500

    feat(fixed testcases): fixed testcases

--- src/components/Menu/_test_/Menu.test.tsx
+++ src/components/Menu/_test_/Menu.test.tsx
@@ -1,5 +1,5 @@
-import { Menu } from '../Menu.component';
-import { MenuItem } from '../MenuItem.component';
+import { Menu  } from '../Menu';
+import { MenuItem } from '../MenuItem';
 import React from 'react';
 import { shallow } from 'enzyme';
 
@@ -20,7 +20,7 @@ test('Menu component should use children prop to show content', () => {
 	const component = shallow(
     <MenuItem title="MenuItem" />
   );
-  expect(component.
+	expect(component.
     props().title).toBe('MenuItem');
 
 });
