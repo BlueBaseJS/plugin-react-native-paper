3841bd27aa5ef0335704487d862cf38325ad934c
adnan1naeem <adnan1naeem@gmail.com>
2019-04-19 17:34:53 +0500

    fix(DialogComponent): dialogComponent  component is updated

--- src/components/Dialog/_stories_/Dialogs.stories.tsx
+++ src/components/Dialog/_stories_/Dialogs.stories.tsx
@@ -1,13 +1,13 @@
 import {
   DialogActionsProps,
   // DialogContentProps,
-  DialogProps,
+  // DialogProps,
+
   DialogTitleProps,
 } from '@bluebase/components';
 
 import AddIcon from '@material-ui/icons/Add';
 import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
 
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -22,57 +22,63 @@ import storiesOf from '@bluebase/storybook-addon';
 import { getComponent } from '@bluebase/core';
 
 
-const Dialog = getComponent<DialogProps>('Dialog');
+const Dialog = getComponent('Dialog');
+const Card = getComponent('Card');
+const View = getComponent('View');
+const Image = getComponent('Image');
+const Button = getComponent('Button');
+
+
 const DialogAction = getComponent<DialogActionsProps>('DialogActions');
 // const DialogContent = getComponent<DialogContentProps>('DialogContent');
 const DialogTitle = getComponent<DialogTitleProps>('DialogTitle');
 
 export interface Ipropsss {
 
-	selectedValue: any,
-	open: any,
-	onClose: any
+  selectedValue: any,
+  open: any,
+  onClose: any
 
 }
 function SimpleDialog(props: Ipropsss) {
-	const { onClose, selectedValue, ...other } = props;
+  const { onClose, selectedValue, ...other } = props;
 
-	function handleClose() {
-		onClose(selectedValue);
-	}
+  function handleClose() {
+    onClose(selectedValue);
+  }
 
-	function handleListItemClick(value: any) {
-		onClose(value);
-	}
+  function handleListItemClick(value: any) {
+    onClose(value);
+  }
 
-	return (
+  return (
     <Dialog visible={true} onDismiss={handleClose} {...other}>
       <DialogTitle>Set backup account</DialogTitle>
-        <List>
-          <ListItem button onClick={() => handleListItemClick('addAccount')}>
-            <ListItemAvatar>
-              <Avatar>
-                <AddIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary="add account" />
-          </ListItem>
-          <ListItem button onClick={() => handleListItemClick('addAccount')}>
-            <ListItemAvatar>
-              <Avatar>
-                <PersonIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary="add account" />
-          </ListItem>
-        </List>
+      <List>
+        <ListItem button onClick={() => handleListItemClick('addAccount')}>
+          <ListItemAvatar>
+            <Avatar>
+              <AddIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="add account" />
+        </ListItem>
+        <ListItem button onClick={() => handleListItemClick('addAccount')}>
+          <ListItemAvatar>
+            <Avatar>
+              <PersonIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="add account" />
+        </ListItem>
+      </List>
       <DialogAction>
         <Button color="primary">
           Save changes
               </Button>
       </DialogAction>
     </Dialog>
-	);
+  );
 }
 
 function SimpleDialogDemo() {
@@ -80,8 +86,8 @@ function SimpleDialogDemo() {
 	const [selectedValue, setSelectedValue] = React.useState();
 
 	function handleClickOpen() {
-		setOpen(true);
-	}
+	setOpen(true);
+}
 
 	const handleClose = (value: any) => {
 		setOpen(false);
@@ -100,11 +106,56 @@ function SimpleDialogDemo() {
 	);
 }
 
-export default SimpleDialogDemo;
 
 storiesOf('Dialog', module)
 
-.add('fullDialog component with all components props', () => (
-  <SimpleDialogDemo />
+  .add('fullDialog component with all components props', () => (
+    <SimpleDialogDemo />
 
-));
+  ));
+
+
+class DialogComponent extends React.Component {
+	state = {
+		visible: false,
+	};
+
+	_hideDialog = () => this.setState({ visible: !this.state.visible });
+
+	render() {
+		return (
+      <>
+        <Dialog
+          visible={this.state.visible}
+          onDismiss={this._hideDialog}
+        >
+
+          <Card visible>
+            <View style={{ display: 'flex', justifyContent: 'center' } as any}>
+              <Image
+                style={{ width: 308, height: 250 } as any}
+                source={{
+                  uri: 'https://www.careerz360.pk/cdn.careerz360.pk/Content/UserData/empr/f1172787-3c92-459c-a5cc-5a9b7da8613e/profile_pics/thumbnail_bd47c439-bf95-4c30-8be8-fe77023194a0.png'
+                }} />
+              <Button style={{ color: 'blue' }} onPress={this._hideDialog}>
+                close
+                </Button>
+            </View>
+          </Card>
+        </Dialog>
+        <Button style={{ marginTop: 200 }} onPress={this._hideDialog}>
+          Open Dialog
+            </Button>
+
+      </>
+		);
+	}
+}
+
+
+storiesOf('Dialog', module)
+
+  .add('fullDialog component with all components props', () => (
+    <DialogComponent />
+
+  ));
--- src/components/Dialog/index.tsx
+++ src/components/Dialog/index.tsx
@@ -1,2 +1,11 @@
-import Dialog from '@material-ui/core/Dialog';
-export { Dialog };
+import { DialogDefaultProps, DialogProps } from '@bluebase/components';
+import MuiDialog from '@material-ui/core/Dialog';
+import { componentMapper } from '@bluebase/component-mapper';
+
+export const Dialog = componentMapper<DialogProps>(MuiDialog, {
+	children: ({ children }: DialogProps) => children,
+	onClose: 'onDismiss',
+	open: 'visible'
+}, { rest: true, });
+
+Dialog.defaultProps = DialogDefaultProps;
