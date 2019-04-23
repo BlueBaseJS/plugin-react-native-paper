8a8bbb2bb2006833073d1f4a694e8515edef23ea
Muhammad Husnain Naseer <mhusnainnaseer786@gmail.com>
2019-04-23 10:30:45 +0500

    fix(Badge Component): Text is removed.
    Children is no more wrapped inside Text component.

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -1,14 +1,10 @@
 import { Badge as RNPBadge } from 'react-native-paper';
 import { BadgeProps } from '@bluebase/components';
 import React from 'react';
-import { getComponent } from '@bluebase/core';
 
-const Text = getComponent('Text');
 const Badge = (props: BadgeProps) => (
-    // const { children, variant, title, variant, ...rest } = props;
-    // const rnpColor = (color === 'secondary') ? theme.palette.secondary.main : theme.palette.primary.main;
     <RNPBadge visible={props.visible} style={props.style}{...props}>
-        <Text>{props.children}</Text>
+        {props.children}
     </RNPBadge>
 );
 
--- src/components/Badge/__stories__/Badge.stories.tsx
+++ src/components/Badge/__stories__/Badge.stories.tsx
@@ -1,19 +1,10 @@
 import { BadgeProps } from '@bluebase/components';
-import React from 'react';
-// import { View } from 'react-native';
-// import { action } from '@storybook/addon-actions';
 import { getComponent } from '@bluebase/core';
 import storiesOf from '@bluebase/storybook-addon';
-// import { withInfo } from '@storybook/addon-info';
-// import { withKnobs } from '@storybook/addon-knobs';
+import React from 'react';
 
 const Badge = getComponent<BadgeProps>('Badge');
-
 const stories = storiesOf('Badge', module);
-
-// stories.addDecorator(withInfo);
-// stories.addDecorator(withKnobs);
-
 stories
 
     .add('Badge with Text', () => (
@@ -23,5 +14,5 @@ stories
         <Badge visible={true} style={{ marginTop: 150, color: 'white', backgroundColor: 'blue' }}>9</Badge>
     ))
     .add('Badge with Red Background Color', () => (
-        <Badge visible={true} style={{ marginTop: 150, backgroundColor: 'red' }}>349</Badge>
+        <Badge visible={true} style={{ marginRight: 50, marginTop: 250, backgroundColor: 'red' }}>349</Badge>
     ))
