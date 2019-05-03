3d11be096a1af14a450bb42a83c26a3d7ddf4d21
Muhammad Husnain Naseer <mhusnainnaseer786@gmail.com>
2019-05-03 17:59:47 +0500

    test(CardHeader): Test cases of CardHeader are added.

--- src/components/CardHeader/_test_/CardHeader.test.tsx
+++ src/components/CardHeader/_test_/CardHeader.test.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 import { mount } from 'enzyme';
 import { waitForElement } from 'enzyme-async-helpers';
 
-test('CardHeader component component should show children', async () => {
+it('CardHeader component should show  full children', async () => {
 	const rendered = mount(
     <BlueBaseApp plugins={[Plugin]}>
       <CardHeader
@@ -24,4 +24,42 @@ test('CardHeader component component should show children', async () => {
 	expect(rendered.find(Card.Title).prop('subtitle')).toBe('description');
 	expect(rendered.find('Button[title="left"]')).toBeTruthy();
 	expect(rendered.find('Button[title="right"]')).toBeTruthy();
+});
+it('CardHeader component should show children without left', async () => {
+	const rendered = mount(
+    <BlueBaseApp plugins={[Plugin]}>
+      <CardHeader
+        title="First Item"
+        description="description"
+        right={<Button title="right" />}
+      />
+    </BlueBaseApp>
+  );
+
+	await waitForElement(rendered, CardHeader);
+
+	// expect(rendered).toMatchSnapshot();
+	expect(rendered.find(Card.Title).prop('subtitle')).toBe('description');
+	expect(rendered.find('Button[title="left"]')).toEqual({});
+  expect(rendered.find('Button[title="right"]')).toBeTruthy();
+  expect(rendered.props().children.props.title).toEqual("First Item")  
+});
+it('CardHeader component should show children without right', async () => {
+	const rendered = mount(
+    <BlueBaseApp plugins={[Plugin]}>
+      <CardHeader
+        title="First Item"
+        description="description"
+        left={<Button title="left" />}
+      />
+    </BlueBaseApp>
+  );
+
+	await waitForElement(rendered, CardHeader);
+
+	// expect(rendered).toMatchSnapshot();
+	expect(rendered.find(Card.Title).prop('subtitle')).toBe('description');
+	expect(rendered.find('Button[title="left"]')).toBeTruthy();
+  expect(rendered.find('Button[title="right"]')).toEqual({});
+  expect(rendered.props().children.props.description).toEqual("description");  
 });
