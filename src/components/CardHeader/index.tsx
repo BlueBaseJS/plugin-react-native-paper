b7a5bfbaae29be2836fee76e0af432b1f471b566
Muhammad Husnain Naseer <mhusnainnaseer786@gmail.com>
2019-05-03 17:29:13 +0500

    fix(CardHeader): Fixed right and left prop space problem

--- src/components/CardHeader/index.tsx
+++ src/components/CardHeader/index.tsx
@@ -1,8 +1,28 @@
+import React from "react";
 import { Card } from 'react-native-paper';
-import { componentMapper } from '@bluebase/component-mapper';
 
-export const CardHeader = componentMapper(Card.Title, {
-	left: ({ left }) => () => left,
-	right: ({ right }) => () => right,
-	subtitle: 'description',
-}, { rest: true });
+export const CardHeader = (props: any) => {
+	if (props.left === undefined) {
+		return (<Card.Title
+			title={props.title}
+			right={() => props.right}
+			subtitle={props.description}
+			style={props.style}
+		/>);
+	}
+	if (props.right === undefined) {
+		return (<Card.Title
+			title={props.title}
+			left={() => props.left}
+			subtitle={props.description}
+			style={props.style}
+		/>);
+	}
+	return (<Card.Title
+		title={props.title}
+		right={() => props.right}
+		left={() => props.left}
+		subtitle={props.description}
+		style={props.style}
+	/>);
+}
