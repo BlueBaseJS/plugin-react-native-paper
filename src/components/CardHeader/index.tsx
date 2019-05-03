2cd66fed0f7feeb66ad04379553437b5b6d1dd17
Muhammad Husnain Naseer <mhusnainnaseer786@gmail.com>
2019-05-03 18:37:56 +0500

    test(CardHeader): fix tslint issues.

--- src/components/CardHeader/index.tsx
+++ src/components/CardHeader/index.tsx
@@ -1,28 +1,34 @@
-import React from "react";
 import { Card } from 'react-native-paper';
+import React from 'react';
 
 export const CardHeader = (props: any) => {
 	if (props.left === undefined) {
-		return (<Card.Title
-			title={props.title}
-			right={() => props.right}
-			subtitle={props.description}
-			style={props.style}
-		/>);
+		return (
+			<Card.Title
+				title={props.title}
+				right={() => props.right}
+				subtitle={props.description}
+				style={props.style}
+			/>
+		);
 	}
 	if (props.right === undefined) {
-		return (<Card.Title
+		return (
+			<Card.Title
+				title={props.title}
+				left={() => props.left}
+				subtitle={props.description}
+				style={props.style}
+			/>
+		);
+	}
+	return (
+		<Card.Title
 			title={props.title}
+			right={() => props.right}
 			left={() => props.left}
 			subtitle={props.description}
 			style={props.style}
-		/>);
-	}
-	return (<Card.Title
-		title={props.title}
-		right={() => props.right}
-		left={() => props.left}
-		subtitle={props.description}
-		style={props.style}
-	/>);
-}
+		/>
+	);
+};
