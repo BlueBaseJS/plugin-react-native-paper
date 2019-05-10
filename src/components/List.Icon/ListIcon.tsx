02fa17da768256e529b2d1fe71231885a9c4972e
Muhammad Husnain Naseer <mhusnainnaseer786@gmail.com>
2019-05-10 14:42:41 +0500

    fix(List.Icon): List.Icon fixed

--- src/components/List.Icon/ListIcon.tsx
+++ src/components/List.Icon/ListIcon.tsx
@@ -1,6 +1,7 @@
+import { DynamicIcon } from '@bluebase/core/dist/components';
 import { List } from 'react-native-paper';
+import React from 'react';
 import { componentMapper } from '@bluebase/component-mapper';
-
 export const ListIcon = componentMapper(List.Icon, {
-	icon: 'name'
+	icon: () => (name:string, size:number) => <DynamicIcon   type="icon" name={name} size={size} />,
 }, { rest: true });
