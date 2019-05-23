ede7c28913c0bcd82c7e3aded99b6c53db71c4bb
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-05-23 21:40:33 +0500

    style(Button): Fixed code style issues

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,26 +1,10 @@
-import { ButtonProps, DynamicIcon, IconProps } from '@bluebase/components';
 import { ThemeConsumer, ThemeContextData } from '@bluebase/core';
 
-import { Button as RNP_Button } from 'react-native-paper';
+import { Button as BButton } from 'react-native-paper';
+import { ButtonProps } from '@bluebase/components';
 import React from 'react';
 
-// import { componentMapper } from '@bluebase/component-mapper';
-
-// export const Button = componentMapper<ButtonProps>(RNP_Button, {
-// 	mode: 'variant',
-
-// 	// TODO: fix color support
-// 	// color: 'color',
-// 	// disabled: 'disabled',
-// 	// fullWidth: 'fullWidth',
-// 	// size: 'size',
-// 	// variant: 'variant',
-// }, {
-// 	ignore: ['color'],
-// 	rest: true,
-// });
-
-const ButtonComponent = (props: ButtonProps) => (
+export const Button = (props: ButtonProps) => (
 	<ThemeConsumer>
 		{({ theme }: ThemeContextData) => {
 			const { children, color, title, variant, icon, ...rest } = props;
@@ -33,13 +17,10 @@ const ButtonComponent = (props: ButtonProps) => (
 					? (theme.palette as any)[color].main
 					: color;
 			return (
-				<RNP_Button {...rest} mode={variant} color={rnpColor as any}>
+				<BButton {...rest} mode={variant} color={rnpColor as any}>
 					{children || title}
-				</RNP_Button>
+				</BButton>
 			);
 		}}
 	</ThemeConsumer>
 );
-
-export const Button = ButtonComponent;
-// export const Button = React.memo(ButtonComponent);
