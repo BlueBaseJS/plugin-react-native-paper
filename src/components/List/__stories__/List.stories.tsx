43898a80757ea7ae477be3bce8ca6acc24d41c38
Asad Razzaq <asad.razzaq@blueeast.com>
2019-05-27 12:19:11 +0500

    ListItem inset prop added

--- src/components/List/__stories__/List.stories.tsx
+++ src/components/List/__stories__/List.stories.tsx
@@ -7,8 +7,6 @@ import storiesOf from '@bluebase/storybook-addon';
 const ListItem = getComponent<ListItemProps>('ListItem');
 
 storiesOf('List', module)
-
-
 	.add('Basic Example', () => (
 		<List>
 			<List.Subheader>Social Accounts</List.Subheader>
@@ -25,15 +23,30 @@ storiesOf('List', module)
 		</List>
 	))
 
-
 	.add('Basic Example with description', () => (
 		<List>
 			<ListItem
 				title="First Item"
-				left={() => <List.Icon name="folder" />}
+				left={<List.Icon name="folder" />}
 				onPress={console.log}
 				description="description"
 			/>
+		</List>
+	))
 
+	.add('With no icon given but inset true', () => (
+		<List>
+			<List.Subheader>Social Accounts</List.Subheader>
+			<List.Item
+				title="Facebook"
+				left={() => <List.Icon name="facebook" />}
+				onPress={() => console.log('Facebook')}
+			/>
+			<List.Item title="Twitter" inset={true} onPress={() => console.log('Twitter')} />
+			<List.Item
+				title="Google"
+				left={() => <List.Icon color="red" name="google" />}
+				onPress={() => console.log('Google')}
+			/>
 		</List>
 	));
--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,8 +1,20 @@
 import { List } from 'react-native-paper';
-import { componentMapper } from '@bluebase/component-mapper';
+import { ListIcon } from '@bluebase/components';
+import React from 'react';
+// import { componentMapper } from '@bluebase/component-mapper';
 
-export const ListItem = componentMapper(List.Item, {
-	description: 'description',
-	left: ({ left }) => () => left,
-	right: ({ right }) => () => right,
-}, { rest: true });
+// export const ListItem = componentMapper(List.Item, {
+// 	description: 'description',
+// 	left: ({ left }) => () => left,
+// 	right: ({ right }) => () => right,
+// }, { rest: true });
+
+export const ListItem = (props: any) => {
+	return (
+		<List.Item
+			left={props.inset ? () => <ListIcon name="" /> : () => props.left}
+			right={props.right ? () => props.right : undefined}
+			{...props}
+		/>
+	);
+};
