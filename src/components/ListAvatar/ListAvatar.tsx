4dde6c3a49d6a970e93c69e06987a85dc7df32ac
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-10-04 21:32:30 +0500

    chore(ListAvatar): Code cleanup

--- src/components/ListAvatar/ListAvatar.tsx
+++ src/components/ListAvatar/ListAvatar.tsx
@@ -2,18 +2,11 @@ import { Avatar, ListAvatarProps } from '@bluebase/components';
 
 import React from 'react';
 import { TextStyle } from 'react-native';
-import { Theme } from '@bluebase/core';
 
 export interface ListAvatarStyles {
 	root: TextStyle;
 }
 
 export const ListAvatar = (props: ListAvatarProps & { styles: ListAvatarStyles }) => (
-	<Avatar size={40} style={{ margin: 8 }} {...props} />
+	<Avatar size={40} style={{ margin: 8, ...props.style }} {...props} />
 );
-
-ListAvatar.defaultStyles = (theme: Theme) => ({
-	root: {
-		color: theme.palette.action.active,
-	},
-});
