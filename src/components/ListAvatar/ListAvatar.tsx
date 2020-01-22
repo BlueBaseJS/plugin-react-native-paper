83cc9d46d65237548a85ddfcab9cdbbc59d91224
adnan1naeem <adnan1naeem@gmail.com>
2020-01-22 20:53:24 +0500

    fix(ListAvatar): Fixed style issues (#219)

--- src/components/ListAvatar/ListAvatar.tsx
+++ src/components/ListAvatar/ListAvatar.tsx
@@ -3,5 +3,5 @@ import { Avatar, ListAvatarProps } from '@bluebase/components';
 import React from 'react';
 
 export const ListAvatar = (props: ListAvatarProps) => (
-	<Avatar size={40} style={{ margin: 8, ...props.style }} {...props} />
+	<Avatar size={40} {...props} style={{ margin: 8, ...props.style }}  />
 );
