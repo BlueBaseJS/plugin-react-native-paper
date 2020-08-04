fedb909f9a3627db2585ff806f6ed832c82245c0
Abdul Rehman Talat <rehman.talat@gmail.com>
2020-08-04 19:25:20 +0500

    feat: Updated to react-native-paper@v4
    BREAKING CHANGE: Bumped core library to V4

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,36 +1,34 @@
 import { ButtonProps, DynamicIcon, IconProps } from '@bluebase/components';
-import { ThemeConsumer, ThemeContextData } from '@bluebase/core';
 
 import { Button as BButton } from 'react-native-paper';
 import React from 'react';
+import { useTheme } from '@bluebase/core';
 
-export const Button = (props: ButtonProps) => (
-	<ThemeConsumer>
-		{({ theme }: ThemeContextData) => {
-			const { children, color, title, variant, ...rest } = props;
-			const rnpColor =
-				color === 'primary' ||
-				color === 'secondary' ||
-				color === 'error' ||
-				color === 'warning' ||
-				color === 'success'
-					? (theme.palette as any)[color].main
-					: color;
+export const Button = (props: ButtonProps) => {
+	const { theme } = useTheme();
+	const { children, color, title, variant, ...rest } = props;
 
-			let icon;
+	const rnpColor =
+		color === 'primary' ||
+		color === 'secondary' ||
+		color === 'error' ||
+		color === 'warning' ||
+		color === 'success'
+			? (theme.palette as any)[color].main
+			: color;
 
-			if (props.icon) {
-				const iconProps = props.icon;
-				icon = ({ color: iColor, size }: IconProps) => (
-					<DynamicIcon {...iconProps} color={iColor} size={size} />
-				);
-			}
+	let icon;
 
-			return (
-				<BButton {...rest} icon={icon} mode={variant} color={rnpColor as any}>
-					{children || title}
-				</BButton>
-			);
-		}}
-	</ThemeConsumer>
-);
+	if (props.icon) {
+		const iconProps = props.icon;
+		icon = ({ color: iColor, size }: IconProps) => (
+			<DynamicIcon {...iconProps} color={iColor} size={size} />
+		);
+	}
+
+	return (
+		<BButton {...rest} icon={icon} mode={variant} color={rnpColor as any}>
+			{children || title}
+		</BButton>
+	);
+};
