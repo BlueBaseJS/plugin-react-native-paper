7c337640e6269c4403e6c1cef60e5016925cba5d
Abdul Rehman Talat <rehman.talat@gmail.com>
2020-08-04 19:13:31 +0500

    chore: Doesn't register it's own font anymore
    BREAKING CHANGE: The plugin doesn't register it's own MaterialIcons font anymore. Although it was redundant, but we're still marking this as a breaking change.

--- src/index.ts
+++ src/index.ts
@@ -43,21 +43,11 @@ import { BlueBase, BootOptions, createPlugin } from '@bluebase/core';
 import { withPortal } from './withPortal';
 import { withReactNativePaper } from './withReactNativePaper';
 
-// TODO: This is a temp fix for:
-// https://github.com/react-native-community/cli/issues/228
-// import MaterialIcons from 'react-native-vector-icons/Fonts/MaterialIcons.ttf';
-// tslint:disable-next-line: no-var-requires
-const MaterialIcons = require('../Fonts/MaterialIcons.ttf');
-
 export default createPlugin({
 	description: 'React Native Paper comes to BlueBase!',
 	key: '@bluebase/plugin-react-native-paper',
 	name: 'React Native Paper',
 	version: '1.0.0',
-
-	assets: {
-		'Material Icons': MaterialIcons,
-	},
 
 	components: {
 		Avatar,
