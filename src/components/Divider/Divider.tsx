fb0aeceddae5cee60e4eaafa5bbae34d396414c8
Abdul Rehman Talat <rehman.talat@gmail.com>
2022-08-30 22:02:17 +0500

    fix(Divider): Improved styles

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -1,3 +1,21 @@
+import { DividerProps } from '@bluebase/components';
 import { Divider as RNPDivider } from 'react-native-paper';
+import React from 'react';
+import { useTheme } from '@bluebase/core';
 
-export const Divider: any = RNPDivider;
+export const Divider = (props: DividerProps) => {
+	const { theme } = useTheme();
+
+	return (
+		<RNPDivider
+			{...props}
+			style={[
+				props.style,
+				{
+					backgroundColor: theme.palette.divider,
+					height: 1,
+				 },
+			]}
+		/>
+	);
+};
